82b117774fa11cf44be641c78d124da7e1eb141c
Hasib105 <101237375+Hasib105@users.noreply.github.com>
2022-05-08 19:57:17 +0600

    Add files via upload

---
+++ Stacks/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] arsgs)
+    {
+        String str="abcd";
+
+        StringReverser reverser=new StringReverser();
+        var result=reverser.reverse(str);
+        System.out.println(result);
+    }
+}
